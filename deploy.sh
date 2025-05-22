9c49f38f0793f67f42ece11698a9b995c3e4d901
Nathanial Myers <nmyers322@gmail.com>
2025-05-22 02:55:40 -0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# Variables
+APP_DIR="/root/helios-api"
+DEPLOY_DIR="/opt/apps/helios-api"
+ENV_FILE="$APP_DIR/.env"
+
+echo "Pulling latest code..."
+cd $APP_DIR
+git pull
+
+echo "Building app..."
+npm install
+npm run build
+
+echo "Copying .env to build directory..."
+cp $ENV_FILE $APP_DIR/build/.env
+
+echo "Installing production dependencies in build..."
+cd $APP_DIR/build
+npm ci --omit=dev
+
+echo "Deploying to $DEPLOY_DIR..."
+rm -rf $DEPLOY_DIR
+mv $APP_DIR/build $DEPLOY_DIR
+
+echo "Restarting PM2 app..."
+cd $DEPLOY_DIR
+pm2 restart helios-api
+
+echo "Deployment complete!"
